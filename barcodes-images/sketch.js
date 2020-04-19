e00ba3dfd9e98b7019b83ded54989e287613fe54
Jacob <jacobcassell94@gmail.com>
2020-04-19 14:50:01 -0700

    don't know what changed

--- barcodes-images/sketch.js
+++ barcodes-images/sketch.js
@@ -1,9 +1,9 @@
-/* 
-This is a blank JavaScript sketch in P5! 
+/*
+This is a blank JavaScript sketch in P5!
 We won't be using this for the AR demo, but this is a GREAT place to learn how to code.
 
 If you are interested in JavaScript, follow Dan Schiffman's YouTube Tutorial Series here:
-https://www.youtube.com/watch?v=yPWkPOfnGsw&list=PLRqwX-V7Uu6Zy51Q-x9tMWIv9cueOFTFA 
+https://www.youtube.com/watch?v=yPWkPOfnGsw&list=PLRqwX-V7Uu6Zy51Q-x9tMWIv9cueOFTFA
 
 For the AR demo, hit the bracket arrow ">" next to where it says "sketch.js" at the top left of this screen. Then open the index.html file.
 
